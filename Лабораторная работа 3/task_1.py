0e31f61b58627104f201e872216312269c99ec26
Sharksu <118103895+Sharksu@users.noreply.github.com>
2023-02-05 14:50:07 +0300

    Update task_1.py

--- Лабораторная работа 3/task_1.py
+++ Лабораторная работа 3/task_1.py
@@ -27,13 +27,11 @@ class PaperBook(Book):
     """Дочерний класс - Бумажная книга"""
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        self._name = name
-        self._author = author
         self.pages = pages
 
     def __str__(self):
         """ Магический метод __str__. Перегружаем, поскольку добавляется новая информация - число страниц"""
-        return f"Книга {self._name}. Автор {self._author}. Количество страниц {self._pages}"
+        return f"Книга {self._name}. Автор {self._author}. Количество страниц {self.pages}"
 
     def __repr__(self) -> str:
         """ Магический метод __repr__. Перегружаем, поскольку добавляется новая информация - число страниц"""
@@ -42,7 +40,7 @@ class PaperBook(Book):
     @property
     def pages(self) -> int:
         """Возвращает количество страниц в книге."""
-        return self._pages
+        return self.pages
 
     @pages.setter
     def pages(self, new_pages: int) -> None:
@@ -51,15 +49,13 @@ class PaperBook(Book):
             raise TypeError("Количество страниц должно быть типа int")
         if new_pages <= 0:
             raise ValueError("Количество страниц должно быть положительным числом")
-        self._pages = new_pages
+        self.pages = new_pages
 
 
 class AudioBook(Book):
     """Дочерний класс - аудио книга"""
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        self._name = name
-        self._author = author
         self.duration = duration
 
     def __str__(self):
@@ -68,12 +64,12 @@ class AudioBook(Book):
 
     def __repr__(self) -> str:
         """ Магический метод __repr__. Перегружаем, поскольку добавляется новая информация - длительность"""
-        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self._duration!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
 
     @property
     def duration(self) -> float:
         """Возвращает длительность книги."""
-        return self._duration
+        return self.duration
 
     @duration.setter
     def duration(self, new_duration: float) -> None:
@@ -82,5 +78,5 @@ class AudioBook(Book):
             raise TypeError("Длительность должна быть типа float")
         if new_duration <= 0:
             raise ValueError("Длительность должна быть положительным числом")
-        self._duration = new_duration
+        self.duration = new_duration
 
